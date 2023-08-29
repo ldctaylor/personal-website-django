8a9f22c81297419ad636f92b200e18ebe8d35b98
Leah <ldctaylor@gmail.com>
2023-08-29 20:32:03 +1000

    Add sign in, out, up links to nav template

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'projects',
     'blog',
     'users',
-    'crispy_forms'
+    'crispy_forms',
     'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,6 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
