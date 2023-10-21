c1389396e3e933ca0e5953cd56048748044968aa
Leah <ldctaylor@gmail.com>
2023-10-22 08:37:15 +1100

    Minor layout amendments to home page

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,10 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9*#=tc&g#(l!1$oi5_h-e&)+n##3df-kh0ehbg)!l2e*sk112e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
