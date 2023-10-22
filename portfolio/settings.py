1a7d2be6750a6cc8464313c5afd35000876091aa
Leah <ldctaylor@gmail.com>
2023-10-22 20:18:11 +1100

    Add custom 404 page to each app

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9*#=tc&g#(l!1$oi5_h-e&)+n##3df-kh0ehbg)!l2e*sk112e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
